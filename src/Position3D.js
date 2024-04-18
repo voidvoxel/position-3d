e23d091cc21db25cc97598961bdd0a1512b43092
Ashlynn "Void Voxel" Juniper <160202125+voidvoxel@users.noreply.github.com>
2024-04-18 16:52:11 -0400

    Add method `Position3D.from` and static method `Position3D.from`

--- src/Position3D.js
+++ src/Position3D.js
@@ -1,4 +1,9 @@
 class Position3D {
+    static from (data) {
+        return new Position3D().from(data);
+    }
+
+
     static one () {
         return new Position3D(1);
     }
@@ -10,6 +15,58 @@ class Position3D {
 
 
     constructor (x, y, z) {
+        this._fromValues(x, y, z);
+    }
+
+
+    from (position) {
+        if (typeof position === 'undefined' || position === null) {
+            position = 0.0;
+        } else if (typeof position === 'boolean') {
+            position = position ? 1.0 : 0.0;
+        } else if (typeof position === 'string') {
+            try {
+                return this.from(
+                    JSON.parse(position)
+                );
+            } catch {
+                position = 0.0;
+            }
+        }
+
+        if (typeof position === 'number') {
+            this._fromValues(position);
+        } else if (typeof position === 'object') {
+            if (position instanceof Array) {
+                this._fromValues(...position);
+            } else if (position.hasOwnProperty('x')) {
+                this.x
+                    = typeof position.x === 'number'
+                    ? position.x
+                    : 0.0
+                ;
+
+                this.y
+                    = typeof position.y === 'number'
+                    ? position.y
+                    : x
+                ;
+
+                this.z
+                    = typeof position.z === 'number'
+                    ? position.z
+                    : x
+                ;
+            }
+        } else {
+            throw new Error("Not yet implemented.");
+        }
+
+        return this;
+    }
+
+
+    _fromValues (x, y, z) {
         this.x = x ?? 0.0;
         this.y = y ?? x;
         this.z = z ?? x;
--- tests/Position3D.test.js
+++ tests/Position3D.test.js
@@ -14,6 +14,118 @@ test(
 
 
 test(
+    `Position3D.from(data: boolean)`,
+    () => {
+        const position = Position3D.from(true);
+
+        expect(position.x).toBe(1.0);
+        expect(position.y).toBe(1.0);
+        expect(position.z).toBe(1.0);
+    }
+);
+
+
+test(
+    `Position3D.from(data: number)`,
+    () => {
+        const position = Position3D.from(42.0);
+
+        expect(position.x).toBe(42.0);
+        expect(position.y).toBe(42.0);
+        expect(position.z).toBe(42.0);
+    }
+);
+
+
+test(
+    `Position3D.from(data: object)`,
+    () => {
+        const position = Position3D.from(
+            {
+                x: 1,
+                y: 2,
+                z: 3
+            }
+        );
+
+        expect(position.x).toBe(1);
+        expect(position.y).toBe(2);
+        expect(position.z).toBe(3);
+    }
+);
+
+
+test(
+    `Position3D.from(data: string) - number`,
+    () => {
+        const positionNumber = Position3D.from("42");
+
+        expect(positionNumber.x).toBe(42);
+        expect(positionNumber.y).toBe(42);
+        expect(positionNumber.z).toBe(42);
+    }
+);
+
+
+test(
+    `Position3D.from(data: string) - object`,
+    () => {
+        const positionJSONObject = Position3D.from(
+            JSON.stringify(
+                {
+                    x: 10,
+                    y: 20,
+                    z: 30
+                }
+            )
+        );
+
+        expect(positionJSONObject.x).toBe(10);
+        expect(positionJSONObject.y).toBe(20);
+        expect(positionJSONObject.z).toBe(30);
+    }
+);
+
+
+test(
+    `Position3D.from(data: string) - Array`,
+    () => {
+        const positionJSONArray = Position3D.from(
+            JSON.stringify(
+                [
+                    11,
+                    22,
+                    33
+                ]
+            )
+        );
+
+        expect(positionJSONArray.x).toBe(11);
+        expect(positionJSONArray.y).toBe(22);
+        expect(positionJSONArray.z).toBe(33);
+    }
+);
+
+
+test(
+    `Position3D.from(data: Array)`,
+    () => {
+        const position = Position3D.from(
+            [
+                2,
+                3,
+                4
+            ]
+        );
+
+        expect(position.x).toBe(2);
+        expect(position.y).toBe(3);
+        expect(position.z).toBe(4);
+    }
+);
+
+
+test(
     `Position3D.one()`,
     () => {
         const position = Position3D.one();
