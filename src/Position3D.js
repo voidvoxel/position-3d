34fb10f1febefff4206d558209188f7a7ebf9bb5
Ashlynn "Void Voxel" Juniper <160202125+voidvoxel@users.noreply.github.com>
2024-04-18 20:12:37 -0400

    Document code

--- src/Position3D.js
+++ src/Position3D.js
@@ -1,4 +1,17 @@
+/**
+ * @typedef {Position3D|Array|object|number|string|boolean} Position3DLike
+ */
+
+
+/**
+ * A position in 3-dimensional space.
+ */
 class Position3D {
+    /**
+     *
+     * @param {Position3D} srcPosition The `Position3D` to copy from.
+     * @param {Position3D} destPosition The `Position3D` to copy to.
+     */
     static copy (
         srcPosition,
         destPosition
@@ -7,36 +20,74 @@ class Position3D {
     }
 
 
+    /**
+     * Create a new `Position3D` from `data`.
+     * @param {Position3DLike} data
+     * @returns {Position3D}
+     */
     static from (data) {
         return new Position3D().from(data);
     }
 
 
+    /**
+     * Returns a `Position3D` with all values set to `1`.
+     * @returns {Position3D} All ones.
+     */
     static one () {
         return new Position3D(1);
     }
 
 
+    /**
+     *
+     * @param {Position3D} position The position to convert into an array.
+     * @returns {Array} The `Position3D` represented as a an array.
+     */
     static toArray (position) {
         return position.toArray();
     }
 
 
+    /**
+     * Returns a `Position3D` with all values set to `0`.
+     * @returns {Position3D} All zeroes.
+     */
     static zero () {
         return new Position3D(0);
     }
 
 
-    constructor (x, y, z) {
+    /**
+     * Create a new `Position3D`.
+     * @param {number} x The x coordinate.
+     * @param {number} y The y coordinate.
+     * @param {number} z The z coordinate.
+     */
+    constructor (
+        x = 0,
+        y = null,
+        z = null
+    ) {
         this._fromValues(x, y, z);
     }
 
 
+    /**
+     * Clone this `Position3D` creating a new instance containing the same
+     * values.
+     * @returns {Position3D} A clone of this `Position3D`.
+     */
     clone () {
         return Position3D.from(this);
     }
 
 
+    /**
+     * Copy values from a `Position3DLike` into this `Position3D`.
+     * @param {Position3DLike} position The `Position3DLike` to copy from.
+     * @returns {Position3D} This `Position3D` after copying is complete.
+     */
     from (position) {
         if (typeof position === 'undefined' || position === null) {
             position = 0.0;
@@ -87,6 +138,10 @@ class Position3D {
     }
 
 
+    /**
+     * Convert this `Position3D` into an `Array`.
+     * @returns {Array} This `Position3D` represented as an `Array`.
+     */
     toArray () {
         return [
             this.x,
@@ -96,8 +151,18 @@ class Position3D {
     }
 
 
-    _fromValues (x, y, z) {
-        this.x = x ?? 0.0;
+    /**
+     * Populate the values of this `Position3D` using the provided arguments.
+     * @param {number} x The x coordinate.
+     * @param {number} y The y coordinate.
+     * @param {number} z The z coordinate.
+     */
+    _fromValues (
+        x = 0,
+        y = null,
+        z = null
+    ) {
+        this.x = x ?? 0;
         this.y = y ?? x;
         this.z = z ?? x;
     }
